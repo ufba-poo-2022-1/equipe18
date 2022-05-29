cb87690ab0c2aa667104298efd036729c633b9dc
Rodrigo Rocha <rodrigorgs@gmail.com>
2022-05-29 09:31:08 -0300

    Adiciona sonar.sh

---
+++ sonar.sh
@@ -0,0 +1,8 @@
+sonar-scanner \
+  -Dsonar.organization=ufba-poo-2022-1 \
+  -Dsonar.projectKey=ufba-poo-2022-1_equipe18 \
+  -Dsonar.sources=. \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.c.file.suffixes=- \
+  -Dsonar.cpp.file.suffixes=- \
+  -Dsonar.objc.file.suffixes=-
